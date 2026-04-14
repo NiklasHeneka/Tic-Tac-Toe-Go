658edfe84ce09c72346e2d915954cfc9629aaa37
Niklas Heneka <93838730+NiklasHeneka@users.noreply.github.com>
2026-04-14 22:43:20 +0200

    Add getting the start order

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func main() {
 
 	fmt.Println("Hello and welcome to a game of Tic-Tac-Toe!")
 	getPlayerName()
+	doesUserStart()
 
 }
 
@@ -34,4 +35,19 @@ func getPlayerName() string {
 		fmt.Println("Invalid input. Please try again.")
 	}
 	return playerName
+}
+
+func doesUserStart() bool {
+	var userStarts string
+
+	for {
+		fmt.Print("Do you want to start the game? (y/n): ")
+		fmt.Scanln(&userStarts)
+
+		if userStarts == "y" {
+			return true
+		} else if userStarts == "n" {
+			return false
+		}
+	}
 }
